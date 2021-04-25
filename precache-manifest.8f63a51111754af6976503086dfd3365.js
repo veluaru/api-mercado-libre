342d027d65907549157b2a2d12c02fb4276bcfda
veluaru <angie.castaneda@udea.edu.co>
2021-04-25 14:40:47 -0500

    deploy

---
+++ precache-manifest.8f63a51111754af6976503086dfd3365.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553ab3a2e9b40b764e71",
+    "url": "/api-mercado-libre/css/app.07bac5b2.css"
+  },
+  {
+    "revision": "69b9b917576e13531e50",
+    "url": "/api-mercado-libre/css/chunk-vendors.1bb1ea3a.css"
+  },
+  {
+    "revision": "136740edfb776e55ddef97fd57744f49",
+    "url": "/api-mercado-libre/index.html"
+  },
+  {
+    "revision": "553ab3a2e9b40b764e71",
+    "url": "/api-mercado-libre/js/app.bbd0bad3.js"
+  },
+  {
+    "revision": "69b9b917576e13531e50",
+    "url": "/api-mercado-libre/js/chunk-vendors.e91343bf.js"
+  },
+  {
+    "revision": "ff4103067f7f0154f658603ebf594536",
+    "url": "/api-mercado-libre/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/api-mercado-libre/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/api-mercado-libre/precache-manifest.8f63a51111754af6976503086dfd3365.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "api-mercado-libre"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
