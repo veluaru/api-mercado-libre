1910504e75bad058983dce7b818b75727a7b11ae
veluaru <angie.castaneda@udea.edu.co>
2021-04-25 14:15:35 -0500

    deployment

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:veluaru/api-mercado-libre.git pwa:gh-pages
+git push -f git@github.com:veluaru/api-mercado-libre.git master:gh-pages
 
 cd -
